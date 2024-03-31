d912e57ffc1fb1abbc93a608e986e050eb051d1a
M B A R K <mbark1236@gmail.com>
2024-03-31 10:26:45 +0000

    Add project files.

---
+++ PgAdminDRPC/PresenceWorker.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using MBDRPC.Core;
+using MBDRPC.Helpers;
+
+namespace PgAdminDRPC
+{
+	public class PresenceWorker
+	{
+		private Presence presence          = new Presence();
+        private bool     isPgAdminFirstRun = true;
+		private DateTime startTime;
+        private string   currentPgAdminProcessName;
+		private string   currentPgAdminAppVersion;
+		    
+
+        private string[] pgAdminProcessNames = { "pgAdmin", "pgAdmin1", "pgAdmin2", "pgAdmin3", "pgAdmin4" };
+
+        private Dictionary<string , string> pgAdminAppNames = new Dictionary<string , string>
+                                                              {
+                                                                  {"pgAdmin" , "pg Admin"} ,
+                                                                  {"pgAdmin1" , "pg Admin 1"} ,
+                                                                  {"pgAdmin2" , "pg Admin 2"} ,
+                                                                  {"pgAdmin3" , "pg Admin 3"} ,
+                                                                  {"pgAdmin4" , "pg Admin 4"} ,
+                                                              };
+
+        public Timer Timer;
+
+
+        public PresenceWorker()
+        {
+            Timer = new Timer(_ => CheckPgAdmin(), null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        }
+
+        /// <summary>
+        /// Starts the presence
+        /// </summary>
+        public void Start()
+		{
+			// Create and start a timer to keep the presence up to date
+			var timer = new Timer(_ => CheckPgAdmin(), null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+
+            timer.Dispose();
+        }
+
+
+
+		private void CheckPgAdmin()
+		{
+			var isPgAdminOpen = RunningAppChecker.IsOneAppRunning(pgAdminProcessNames);
+			if (isPgAdminOpen)
+			{
+				if (isPgAdminFirstRun)
+                {
+                    currentPgAdminProcessName = GetWhichProcessNameIsRunning();
+                    currentPgAdminAppVersion  = GetWhichAppVersionIsRunning();
+                    
+					presence.InitializePresence("1223760050753896548");
+
+					presence.UpdateLargeImage("postgresql", currentPgAdminAppVersion);
+					presence.UpdateDetails(currentPgAdminAppVersion);
+
+
+					startTime         = RunningAppChecker.GetProcessStartTime(currentPgAdminProcessName);
+					isPgAdminFirstRun = false;
+				}
+
+				UpdatePresence();
+			}
+			else
+			{
+				presence.ShutDown();
+				isPgAdminFirstRun = true;
+			}
+		}
+
+
+		/// <summary>
+		/// Updates the presence
+		/// </summary>
+		private void UpdatePresence()
+		{
+			UpdatePresenceTime();
+			presence.UpdatePresence();
+		}
+
+		/// <summary>
+		/// Updates the presence time
+		/// </summary>
+		private void UpdatePresenceTime()
+		{
+			var elapsedTime = (DateTime.Now - startTime).ToString(@"hh\:mm\:ss");
+			presence.UpdateState(elapsedTime);
+		}
+
+
+        private string GetWhichProcessNameIsRunning()
+        {
+            var processName = Process.GetProcesses()
+                                     .FirstOrDefault( p => pgAdminProcessNames.Contains( p.ProcessName ,
+                                                          StringComparer.OrdinalIgnoreCase ) )?.ProcessName ??
+                              string.Empty;
+
+            return processName;
+        }
+
+
+        private string GetWhichAppVersionIsRunning()
+        {
+            return pgAdminAppNames[currentPgAdminProcessName];
+        }
+
+	}
+}
---
+++ PgAdminDRPC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PgAdminDRPC
+{
+    internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var worker = new PresenceWorker();
+
+			worker.Start();
+
+            Console.ReadKey();
+
+			// Dispose the timer
+            worker.Timer.Dispose();
+
+			// Kill the application
+            Environment.Exit(0);
+        }
+	}
+}
